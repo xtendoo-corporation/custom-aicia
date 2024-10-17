cd4f344fb7a47411cf36cfde8f06fd0acaf18529
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-10-17 13:25:08 +0200

    sycn

--- aicia_custom_hr_employee/models/hr_employee.py
+++ aicia_custom_hr_employee/models/hr_employee.py
@@ -1,3 +1,5 @@
+from pygments.lexer import default
+
 from odoo import api, fields, models
 
 
@@ -6,13 +8,10 @@ class HrEmployee(models.Model):
 
     other_field = fields.Many2many('employee.attribute', string='Información adicional')
 
+    # partner_id = fields.Char(string="Contacto")
+
     partner_id = fields.Many2one('res.partner', string="Contacto", compute="_compute_partner_id", store=True)
-    partner_id_2 = fields.Many2one('res.partner', string="Contacto 2", compute="_compute_partner_id_2", store=True)
 
     def _compute_partner_id(self):
-        for record in self:
-            record.partner_id = self.env['res.partner'].search([('id', '=', record.work_contact_id.id)], limit=1)
-
-    def _compute_partner_id_2(self):
-        for record in self:
-            record.partner_id = self.env['res.partner'].search([('work_contact_id', '=', record.id)], limit=1)
+        for record in self: (
+            record.partner_id) = self.env['res.partner'].search([('id', '=', record.work_contact_id.id)], limit=1)
