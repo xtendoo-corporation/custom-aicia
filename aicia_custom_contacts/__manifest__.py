c6300ffb0bfbe4199e639a93ba189a55d1cb36d7
Gokac9 <theabraham9@gmail.com>
2024-10-21 13:09:43 +0200

    Add module to add fields companys in res_partner

---
+++ aicia_custom_contacts/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "aicia_custom_contacts",
+    "summary": """Aicia Custom Contacts""",
+    "version": "17.0.1.0.0",
+    "description": """Aicia Custom Contacts""",
+    "company": "Xtendoo",
+    "website": "http://www.xtendoo.es",
+    "category": "Contacts",
+    "depends": ["contacts"],
+    "license": "AGPL-3",
+    "data": [
+        "views/res_partner_view.xml"
+    ],
+    "installable": True,
+}
