a13eb3a025c66fc159b7312d0261fa8eb20619d2
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-12-20 13:36:09 +0100

    add new module to upload documents

---
+++ aicia_approval_documents/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "aicia_approval_documents",
+    "summary": """Aicia Approval Documents""",
+    "version": "17.0.1.0.0",
+    "description": """Aicia Approval Documents""",
+    "company": "Xtendoo",
+    "website": "http://www.xtendoo.es",
+    "category": "",
+    "depends": ["mail"],
+    "license": "AGPL-3",
+    "data": [
+        "data/type_approval.xml",
+        "views/aicia_approval_documents.xml",
+        "views/aicia_type_approval.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+}
