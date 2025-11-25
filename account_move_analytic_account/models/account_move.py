2bc98eca4c2c3cf918d94d001916bd1e034fc349
dani <dani.xtendoo@gmail.com>
2025-11-25 10:21:52 +0100

    sync

--- account_move_analytic_account/models/account_move.py
+++ account_move_analytic_account/models/account_move.py
@@ -1,6 +1,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class AccountMove(models.Model):
@@ -16,6 +19,31 @@ class AccountMove(models.Model):
         ),
     )
 
+    work_group_id = fields.Many2one(
+        compute="_compute_work_group_id",
+        comodel_name="portal.work.group",
+        string="Grupo de Trabajo",
+        store=False,  # Almacenar para evitar que la primera lectura devuelva `_unknown`
+        compute_sudo=True,  # Calcular con privilegios de superusuario para evitar errores de acceso
+    )
+
+    @api.depends("analytic_distribution")
+    def _compute_work_group_id(self):
+        for record in self:
+            if record.analytic_distribution:
+                print("analytic_distribution",record.analytic_distribution)
+                first_key = next(iter(record.analytic_distribution))
+                if first_key:
+                    account = self.env['account.analytic.account'].browse(int(first_key))
+                    print("first_key",first_key)
+                    if account and account.work_group_id:
+                        record.work_group_id = account.work_group_id
+                else:
+                    record.work_group_id = False
+            else:
+                record.work_group_id = False
+
+
     @api.onchange("analytic_distribution")
     def _onchange_analytic_distribution(self):
         """Actualiza analytic_distribution en todas las líneas del movimiento"""
@@ -25,4 +53,3 @@ class AccountMove(models.Model):
         else:
             for line in self.line_ids:
                 line.analytic_distribution = False
-
