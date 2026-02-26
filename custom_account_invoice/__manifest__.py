a243fa3d0a0d227ac9bf09de377b2955e6b2a2eb
AlvaroRamirez10 <alvaroramirezrodriguez2003@gmail.com>
2026-02-26 13:04:26 +0100

    modificacion de factura

---
+++ custom_account_invoice/__manifest__.py
@@ -0,0 +1,22 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Custom Account Invoice",
+    "summary": "Factura personalizada para AICIA",
+    "version": "19.0.1.0.0",
+    "author": "Xtendoo",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Accounting",
+    "depends": [
+        "account",
+    ],
+    "license": "AGPL-3",
+    "data": [
+        "views/account_move_view.xml",
+        "views/report_invoice.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
